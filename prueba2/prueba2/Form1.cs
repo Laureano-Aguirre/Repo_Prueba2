f346688f6acf8babf2b4bd86f6dc1e3219218a20
Laureano-Aguirre <78192480+Laureano-Aguirre@users.noreply.github.com>
2023-07-24 13:20:21 -0300

    1-commit

---
+++ prueba2/prueba2/Form1.cs
@@ -0,0 +1,15 @@
+namespace prueba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile(@"images\img-Fondo.png");
+        }
+    }
+}
